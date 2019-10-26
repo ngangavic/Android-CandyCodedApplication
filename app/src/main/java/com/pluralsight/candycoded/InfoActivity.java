f86b4f7f37509f77c8909aa6be6b19de57ee5d59
Ng'ang'a Victor <ngangavictor10@gmail.com>
2019-10-26 15:58:53 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -36,7 +36,7 @@ public class InfoActivity extends AppCompatActivity {
     public void createMapIntent(View view){
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW,uri).setPackage("com.google.android.apps.maps");
-        if (mapIntent.resolveActivity(getPackageManager()).equals(null)){
+        if (mapIntent.resolveActivity(getPackageManager())!=null){
             startActivity(mapIntent);
         }
     }
